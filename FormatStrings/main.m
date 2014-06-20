8cdfdec1c3ca0faa80b96ed2c6ccec08b66b6fea
fgsantoyo <fgsantoyo@gmail.com>
2014-06-19 23:24:37 -0400

    Initial Commit

---
+++ FormatStrings/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  FormatStrings
+//
+//  Created by Fernando Garcia, Jr on 6/19/14.
+//  Copyright (c) 2014 Fernando Garcia, Jr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
